573b8609c5bc649dab56c1782a19c047b173fe91
nagode <nagode@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-06 15:29:52 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rebmix@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/rebmix/R/RCLSMIX.R
@@ -0,0 +1,145 @@
+RCLSMIX <- function(object,
+  P = NULL,
+  Dataset = NULL, ...)
+{
+  digits <- getOption("digits"); options(digits = 15)
+
+  if (missing(object)) {
+    stop(sQuote("object"), " object or list of classes REBMIX is requested!", call. = FALSE)
+  }
+
+  if (class(object) == "list") {
+    o <- length(object)
+
+    for (i in 1:o) {
+      if (class(object[[i]]) != "REBMIX") {
+        stop(sQuote("object"), " list of classes REBMIX is requested!", call. = FALSE)
+      }
+    }
+  }
+  else
+  if (class(object) == "REBMIX") {
+    o <- 1;
+
+    object[[1]] <- object
+  }
+  else {
+    stop(sQuote("object"), " object or list of classes REBMIX is requested!", call. = FALSE)
+  }
+
+  if (is.null(Dataset)) {
+    stop(sQuote("Dataset"), " must not be NULL!", call. = FALSE)
+  }
+
+  Dataset <- as.data.frame(Dataset)
+
+  s <- nrow(object[[1]]$summary)
+
+  if (o > 1) {
+    for (i in 2:o) {
+      if (s != nrow(object[[i]]$summary)) {
+        stop(sQuote("object"), " list of classes REBMIX with equal number of classes is requested!", call. = FALSE)
+      }
+    }
+  }
+
+  if (s > 1) {
+    if (is.null(P)) {
+      stop(sQuote("P"), " must not be NULL!", call. = FALSE)
+    }
+
+    if (s != length(P)) {
+      stop(sQuote("object"), " and ", sQuote("P"), " must be of the same length!", call. = FALSE)
+    }
+  }
+
+  d <- array(data = NA, dim = o, dimnames = NULL)
+
+  c <- array(data = list(NULL), dim = c(o, s), dimnames = NULL)
+
+  w <- array(data = list(NULL), dim = c(o, s), dimnames = NULL)
+
+  pdf <- array(data = list(NULL), dim = c(o, s), dimnames = NULL)
+
+  theta1 <- array(data = list(NULL), dim = c(o, s), dimnames = NULL)
+
+  theta2 <- array(data = list(NULL), dim = c(o, s), dimnames = NULL)
+
+  for (io in 1:o) {
+    for (is in 1:s) {
+      nrow <- nrow(object[[io]]$Theta[[is]])
+      ncol <- ncol(object[[io]]$Theta[[is]])
+
+      c[[io, is]] <- ncol
+
+      w[[io, is]] <- as.numeric(object[[io]]$w[[is]])
+
+      pdf[[io, is]] <- array(data = NA, dim = c(nrow, ncol), dimnames = NULL)
+      theta1[[io, is]] <- array(data = 0.0, dim = c(nrow, ncol), dimnames = NULL)
+      theta2[[io, is]] <- array(data = 0.0, dim = c(nrow, ncol), dimnames = NULL)
+
+      for (j in 1:ncol) {
+        M <- match(object[[io]]$Theta[[is]][, j], .rebmix$pdf)
+
+        d[io] <- 1;
+
+        for (l in 1:length(M)) {
+          if (M[l] %in% which(.rebmix$pdf.nargs == 2)) {
+            pdf[[io, is]][d[io], j] <- object[[io]]$Theta[[is]][l, j]
+            theta1[[io, is]][d[io], j] <- as.numeric(object[[io]]$Theta[[is]][l + 1, j])
+            theta2[[io, is]][d[io], j] <- as.numeric(object[[io]]$Theta[[is]][l + 2, j])
+
+            d[io] <- d[io] + 1
+          }
+          else
+          if (M[l] %in% which(.rebmix$pdf.nargs == 1)) {
+            pdf[[io, is]][d[io], j] <- object[[io]]$Theta[[is]][l, j]
+            theta1[[io, is]][d[io], j] <- as.numeric(object[[io]]$Theta[[is]][l + 1, j])
+
+            d[io] <- d[io] + 1
+          }
+        }
+      }
+
+      d[io] <- d[io] - 1
+
+      pdf[[io, is]] <- pdf[[io, is]][1:d[io], ]; dim(pdf[[io, is]]) <- c(d[io], ncol)
+      theta1[[io, is]] <- theta1[[io, is]][1:d[io], ]; dim(theta1[[io, is]]) <- c(d[io], ncol)
+      theta2[[io, is]] <- theta2[[io, is]][1:d[io], ]; dim(theta2[[io, is]]) <- c(d[io], ncol)
+    }
+  }
+
+  if (s > 1) {
+    message("RCLSMIX Version 2.7.0");
+    flush.console()
+
+    output <- .C("RCLSMIX",
+      n = as.integer(nrow(Dataset)),
+      X = as.double(unlist(Dataset)),
+      s = as.integer(s),
+      o = as.integer(o),
+      d = as.integer(d),
+      c = as.integer(unlist(c)),
+      W = as.double(unlist(w)),
+      ParFamType = as.character(unlist(pdf)),
+      Par0 = as.double(unlist(theta1)),
+      Par1 = as.double(unlist(theta2)),
+      P = as.double(unlist(P)),
+      Z = integer(nrow(Dataset)),
+      error = integer(1),
+      PACKAGE = "rebmix")
+
+    if (output$error == 1) {
+      stop("in RCLSMIX!", call. = FALSE); return(NA)
+    }
+  }
+
+  output <- as.factor(output$Z)
+  levels(output) <- 0:(length(P) -1)
+  
+  options(digits = digits)
+
+  rm(list = ls()[!(ls() %in% c("output"))])
+
+  return(output)
+} ## RCLSMIX
---
+++ pkg/rebmix/R/boot.REBMIX.R
@@ -0,0 +1,205 @@
+boot.REBMIX <- function(x,
+  pos = 1,
+  Bootstrap = "parametric",
+  B = 100, 
+  n = NULL,
+  replace = TRUE, 
+  prob = NULL, ...)
+{
+  digits <- getOption("digits"); options(digits = 15)
+  
+  if (missing(x) || (class(x) != "REBMIX")) {
+    stop(sQuote("x"), " object of class REBMIX is requested!", call. = FALSE)
+  }
+
+  if (!is.wholenumber(pos)) {
+    stop(sQuote("pos"), " integer is requested!", call. = FALSE)
+  }
+
+  if ((pos < 1) || (pos > nrow(x$summary))) {
+    stop(sQuote("pos"), " must be greater than 0 and less or equal than ", nrow(x$summary), "!", call. = FALSE)
+  }
+  
+  if (!is.character(Bootstrap)) {
+    stop(sQuote("Bootstrap"), " character is requested!", call. = FALSE)
+  }
+
+  Bootstrap <- match.arg(Bootstrap, .rebmix.boot$Bootstrap, several.ok = FALSE)    
+  
+  if (!is.wholenumber(B)) {
+    stop(sQuote("B"), " integer is requested!", call. = FALSE)
+  }
+
+  if (B < 1) {
+    stop(sQuote("B"), " must be greater than 0!", call. = FALSE)
+  }
+  
+  nmax <- nrow(as.matrix(x$Dataset[[which(names(x$Dataset) == x$summary[pos, "Dataset"])]]))
+  
+  if (is.null(n)) {
+    n <- nmax
+  }
+  else {
+    if (!is.wholenumber(n)) {
+      stop(sQuote("n"), " integer is requested!", call. = FALSE)
+    }
+  
+    if ((n < 1) || (n > nmax)) {
+      stop(sQuote("n"), " must be greater than 0 and less or equal than ", nmax, "!", call. = FALSE)
+    }
+  }  
+  
+  if (Bootstrap == .rebmix.boot$Bootstrap[1]) {
+    bsample <- RNGMIX(Dataset = paste("bsample_", 1:B, sep = ""),
+      n = rbind(ceiling(n * as.numeric(x$w[[pos]]))),
+      Theta = x$Theta[[pos]], ...)
+  }
+  else
+  if (Bootstrap == .rebmix.boot$Bootstrap[2]) {
+    Dataset <- as.matrix(x$Dataset[[which(names(x$Dataset) == x$summary[pos, "Dataset"])]])
+    
+    bsample <- list()
+    
+    bsample$Dataset <- list()
+    
+    range <- NULL
+
+    for (i in 1:B) {
+      R1 <- sample.int(nmax, size = n, replace = replace, prob = prob)
+      
+      bsample$Dataset[[i]] <- as.data.frame(Dataset[R1, ], stringsAsFactors = FALSE)
+      
+      range <- apply(rbind(range, Dataset[R1, , drop = FALSE]), 2, range)
+    }
+    
+    bsample$ymin <- range[1, ]; bsample$ymax <- range[2, ]
+  }
+    
+  d <- length(x$call$pdf)
+  
+  bsampleest <- REBMIX(Dataset = bsample$Dataset,
+    Preprocessing = as.character(x$summary[pos, "Preprocessing"]),
+    cmax = as.numeric(x$summary[pos, "cmax"]),
+    Criterion = as.character(x$summary[pos, "Criterion"]),
+    Variables = x$call$Variables,
+    pdf = x$call$pdf,
+    Theta1 = x$call$Theta1,
+    Theta2 = x$call$Theta2,
+    K = eval(parse(text = as.character(x$summary[pos, "K"]))),
+    y0 = x$call$y0,    
+    ymin = x$call$ymin,
+    ymax = x$call$ymax,
+    ar = as.numeric(x$summary[pos, "ar"]),
+    Restraints = as.character(x$summary[pos, "Restraints"]))
+
+  freq <- table(as.numeric(bsampleest$summary$c))
+  c <- as.integer(names(freq)[which.max(freq)])
+  
+  w <- rbind(bsampleest$w[as.numeric(bsampleest$summary$c) == c])
+  
+  Theta <- rbind(bsampleest$Theta[as.numeric(bsampleest$summary$c) == c])
+  
+  output <- list()
+  
+  output$c <- as.data.frame(rbind(as.numeric(bsampleest$summary$c)), stringsAsFactors = FALSE)
+  output$c.se <- sd(as.numeric(bsampleest$summary$c))
+  output$c.cv <- output$c.se / mean(as.numeric(bsampleest$summary$c))
+  
+  rownames(output$c) <- "c"
+  colnames(output$c) <- paste(1:B, sep = "")  
+  
+  output$c.mode <- c
+  output$c.prob <- length(w) / B
+  
+  output$w <- NULL
+  output$w.se <- NULL
+  
+  for (i in 1:length(w)) {
+    output$w <- cbind(output$w, as.numeric(w[[i]]))
+  }
+  
+  output$w.se <- apply(output$w, 1, sd)
+  output$w.cv <- output$w.se / apply(output$w, 1, mean)
+  
+  output$w <- as.data.frame(rbind(output$w), stringsAsFactors = FALSE)
+  
+  rownames(output$w) <- paste("comp", if (c > 1) 1:c else "", sep = "")
+  colnames(output$w) <- paste(which(bsampleest$summary$c == c), sep = "") 
+  
+  nrow <- nrow(Theta[[1]])
+  ncol <- ncol(Theta[[1]])  
+  
+  R1 <- NULL; R2 <- NULL; D1 <- NULL; D2 <- NULL
+  
+  i <- 1; j <- 1
+    
+  while (i < nrow) {
+    pdf <- match.arg(Theta[[1]][i, 1], .rebmix$pdf)
+
+    if (pdf %in% .rebmix$pdf[.rebmix$pdf.nargs == 2]) {
+      R1 <- c(R1, i + 1); R2 <- c(R2, i + 2); D1 <- c(D1, j); D2 <- c(D2, j)
+      
+      i <- i + 3; j <- j + 1
+    }
+    else
+    if (pdf %in% .rebmix$pdf[.rebmix$pdf.nargs == 1]) {
+      R1 <- c(R1, i + 1); D1 <- c(D1, j)
+      
+      i <- i + 2; j <- j + 1
+    }
+  }
+  
+  for (i in 1:length(R1)) {
+    theta1 <- if (d > 1) paste("theta1.",  D1[i], sep = "") else "theta1"
+    
+    output[[theta1]] <- NULL
+
+    for (j in 1:length(Theta)) {
+      output[[theta1]] <- cbind(output[[theta1]], as.numeric(Theta[[j]][R1[i], ]))
+    }
+    
+    se <- apply(output[[theta1]], 1, sd)
+
+    output[[paste(theta1, ".se", sep = "")]] <- se
+
+    cv <- se / apply(output[[theta1]], 1, mean)
+    
+    output[[paste(theta1, ".cv", sep = "")]] <- cv
+    
+    output[[theta1]] <- data.frame(rbind(output[[theta1]]), stringsAsFactors = FALSE)
+    
+    rownames(output[[theta1]]) <- paste("comp", if (c > 1) 1:c else "", sep = "")
+    colnames(output[[theta1]]) <- paste(which(bsampleest$summary$c == c), sep = "") 
+  }
+  
+  for (i in 1:length(R2)) {
+    theta2 <- if (d > 1) paste("theta2.",  D2[i], sep = "") else "theta2"
+    
+    output[[theta2]] <- NULL
+
+    for (j in 1:length(Theta)) {
+      output[[theta2]] <- cbind(output[[theta2]], as.numeric(Theta[[j]][R2[i], ]))
+    }
+    
+    se <- apply(output[[theta2]], 1, sd)
+
+    output[[paste(theta2, ".se", sep = "")]] <- se
+
+    cv <- se / apply(output[[theta2]], 1, mean)
+    
+    output[[paste(theta2, ".cv", sep = "")]] <- cv
+    
+    output[[theta2]] <- data.frame(rbind(output[[theta2]]), stringsAsFactors = FALSE)
+    
+    rownames(output[[theta2]]) <- paste("comp", if (c > 1) 1:c else "", sep = "")
+    colnames(output[[theta2]]) <- paste(which(bsampleest$summary$c == c), sep = "") 
+  }  
+  
+  options(digits = digits)  
+
+  rm(list = ls()[!(ls() %in% c("output"))]) 
+  
+  class(output) <- "boot.REBMIX" 
+   
+  return(output)  
+} ## boot.REBMIX
